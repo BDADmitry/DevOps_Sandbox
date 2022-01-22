fb02c1f3616a1e202159831d25221b311c0658ef
Dmitry Bogdanov <deman27@mail.ru>
2022-01-22 13:49:06 +0300

    test script

---
+++ test_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Enter file name:" FILENAME
+
+if [ -f $FILENAME ]; then
+    echo "File $FILENAME exists in $(pwd)."
+else
+    touch $FILENAME
+    echo "$FILENAME created in $(pwd)"
+    ps -e --sort pid > $FILENAME
+    echo "Processes sorting finished."
+fi
+
